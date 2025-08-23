ba71ee5827a53e69b4469303b9099d7454675c10
wolfen-soul <195895194+wolfen-soul@users.noreply.github.com>
2025-08-23 14:25:19 +0300

    Removed game logic due to complete rewriting

--- src/main/java/com/example/backend/security/SecurityConfig.java
+++ src/main/java/com/example/backend/security/SecurityConfig.java
@@ -17,10 +17,7 @@ import com.example.backend.application.UserPrincipalService;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Configuration
