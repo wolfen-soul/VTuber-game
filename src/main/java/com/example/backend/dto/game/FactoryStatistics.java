2c3c3e2208e6c8364b8d8deaf332264910dd4e0c
wolfen-soul <195895194+wolfen-soul@users.noreply.github.com>
2025-09-16 15:51:22 +0300

    cleanup: duplicate files removed

--- src/main/java/com/example/backend/dto/game/FactoryStatistics.java
+++
@@ -1,4 +0,0 @@
-package com.example.backend.dto.game;
-
-public record FactoryStatistics(int clicks, int income) {
-}
--- src/main/java/com/example/backend/dto/game/GameStateDto.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.dto.game;
-
-import java.util.List;
-
-public record GameStateDto(String nickname, int subscribers, int streamsAmount, int averageOnline, int balance,
-                           int donationsAmount, String registrationDate, Integer collaborationStreams,
-                           AgencyDto agency, CollabDto collab, List<ItemDto> ownedItems, boolean activeStream) {}
